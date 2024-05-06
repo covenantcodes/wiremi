36d819faa78e3c1947864e73ccbf262076102207
Covenant Ifeoluwa <covenantifeoluwa4@gmail.com>
2024-05-06 21:10:34 +0100

    Updated semi circle styles

--- src/Auth/Login.jsx
+++ src/Auth/Login.jsx
@@ -8,6 +8,13 @@ const Login = () => {
       <div className="semi-circle bottom-right"></div>
 
       {/* LOGIN FORM */}
+      <div className="login_container">
+            <div className="login_box">
+                <div className="login_title">Login</div>
+            </div>
+            <div className="login_box">
+            </div>
+      </div>
     </div>
   );
 };
