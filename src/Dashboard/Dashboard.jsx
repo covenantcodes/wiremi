28f456be57023ebf9b8643d4a825fbab0650d0ca
Covenant Ifeoluwa <covenantifeoluwa4@gmail.com>
2024-05-09 10:13:17 +0100

    Started with KYC Stats container

--- src/Dashboard/Dashboard.jsx
+++ src/Dashboard/Dashboard.jsx
@@ -7,9 +7,10 @@ import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
 import { faBell } from "@fortawesome/free-regular-svg-icons";
 import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
 import { faCaretDown } from "@fortawesome/free-solid-svg-icons";
+import { faCircle } from "@fortawesome/free-solid-svg-icons";
 import CustomButton from "../Components/CustomButton";
 import CustomSideBar from "../Components/CustomSideBar/CustomSideBar";
-
+import CountUp from "react-countup";
 import { useProSidebar } from "react-pro-sidebar";
 
 const Dashboard = () => {
@@ -155,7 +156,9 @@ const Dashboard = () => {
                   <img src="../../img/download.png" className="stats_img" />
                 </div>
 
-                <div className="stats_count">6784</div>
+                <div className="stats_count">
+                  <CountUp start={0} end={4354} duration={3} />
+                </div>
 
                 <div className="bottom_stats_count">
                   <span>
@@ -163,20 +166,22 @@ const Dashboard = () => {
                     <FontAwesomeIcon
                       icon={faCaretDown}
                       fontSize={16}
-                      style={{paddingLeft: ".3rem"}}
+                      style={{ paddingLeft: ".3rem" }}
                     />
                   </span>
                   +30 today
                 </div>
               </div>
 
-                 <div className="stats_box">
+              <div className="stats_box">
                 <div className="stats_header">
                   Total Verified Users
                   <img src="../../img/verified.png" className="stats_img" />
                 </div>
 
-                <div className="stats_count">6784</div>
+                <div className="stats_count">
+                  <CountUp start={0} end={6758} duration={3} />
+                </div>
 
                 <div className="bottom_stats_count">
                   <span>
@@ -184,20 +189,22 @@ const Dashboard = () => {
                     <FontAwesomeIcon
                       icon={faCaretDown}
                       fontSize={16}
-                      style={{paddingLeft: ".3rem"}}
+                      style={{ paddingLeft: ".3rem" }}
                     />
                   </span>
                   +100 today
                 </div>
               </div>
 
-                 <div className="stats_box">
+              <div className="stats_box">
                 <div className="stats_header">
                   Total Agents
                   <img src="../../img/agents.png" className="stats_img" />
                 </div>
 
-                <div className="stats_count">6784</div>
+                <div className="stats_count">
+                  <CountUp start={0} end={1245} duration={3} />
+                </div>
 
                 <div className="bottom_stats_count">
                   <span>
@@ -205,20 +212,22 @@ const Dashboard = () => {
                     <FontAwesomeIcon
                       icon={faCaretDown}
                       fontSize={16}
-                      style={{paddingLeft: ".3rem"}}
+                      style={{ paddingLeft: ".3rem" }}
                     />
                   </span>
-                 +$150 today
+                  +$150 today
                 </div>
               </div>
 
-                 <div className="stats_box">
+              <div className="stats_box">
                 <div className="stats_header">
                   Total Organization
                   <img src="../../img/organization.png" className="stats_img" />
                 </div>
 
-                <div className="stats_count">6784</div>
+                <div className="stats_count">
+                  <CountUp start={0} end={457} duration={3} />
+                </div>
 
                 <div className="bottom_stats_count">
                   <span>
@@ -226,7 +235,7 @@ const Dashboard = () => {
                     <FontAwesomeIcon
                       icon={faCaretDown}
                       fontSize={16}
-                      style={{paddingLeft: ".3rem"}}
+                      style={{ paddingLeft: ".3rem" }}
                     />
                   </span>
                   +$150 today
@@ -234,8 +243,148 @@ const Dashboard = () => {
               </div>
             </div>
 
-            <div className="kyc_status_container">
-                
+            <div className="kyc_status_main">
+              <div className="kyc_status_container">
+                <div className="kyc_status_container_table">
+                  <table>
+                    <thead>
+                      <tr className="table_head_row">
+                        <th>SL</th>
+                        <th>Name</th>
+                        <th> Date</th>
+                        <th>Request ID</th>
+                        <th>Documents</th>
+                        <th>KYC Status</th>
+                        <th></th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>1</td>
+                        <td>Shivashankar Prasad</td>
+                        <td>23/05/20</td>
+                        <td>AF23048421</td>
+                        <td>2/3</td>
+                        <td className="icon-text-td">
+                          <FontAwesomeIcon
+                            icon={faCircle}
+                            color="var(--global-red)"
+                            fontSize={7}
+                            style={{
+                              display: "flex",
+                              alignItems: "center",
+                              justifyContent: "center",
+                            }}
+                          />
+                          Incomplete
+                        </td>
+                        <td>View</td>
+                      </tr>
+
+                      <tr>
+                        <td>2</td>
+                        <td>Ravi Shankar Shukla</td>
+                        <td>09/02/20</td>
+                        <td>AF25142455</td>
+                        <td>3/3</td>
+                        <td className="icon-text-td">
+                          <FontAwesomeIcon
+                            icon={faCircle}
+                            color="#2FE784"
+                            fontSize={7}
+                            style={{
+                              display: "flex",
+                              flexDirection: "row",
+                              alignItems: "center",
+                              justifyContent: "center",
+                            }}
+                          />
+                          Complete
+                        </td>
+                        <td>View</td>
+                      </tr>
+
+                      <tr>
+                        <td>3</td>
+                        <td>Akhandanand Triphali</td>
+                        <td>23/10/20</td>
+                        <td>AF27241245</td>
+                        <td>3/3</td>
+                        <td className="icon-text-td">
+                          <FontAwesomeIcon
+                            icon={faCircle}
+                            color="#2FE784"
+                            fontSize={7}
+                            style={{
+                              display: "flex",
+                              alignItems: "center",
+                              justifyContent: "center",
+                            }}
+                          />
+                          Complete
+                        </td>
+                        <td>View</td>
+                      </tr>
+
+                      <tr>
+                        <td>4</td>
+                        <td>Balraj Sethi</td>
+                        <td>07/04/20</td>
+                        <td>AF23048421</td>
+                        <td>1/3</td>
+                        <td className="icon-text-td">
+                          <FontAwesomeIcon
+                            icon={faCircle}
+                            color="var(--global-red)"
+                            fontSize={7}
+                            style={{
+                              display: "flex",
+                              alignItems: "center",
+                              justifyContent: "center",
+                            }}
+                          />
+                          Incomplete
+                        </td>
+                        <td>View</td>
+                      </tr>
+
+                      <tr>
+                        <td>5</td>
+                        <td>Guddu Pandit</td>
+                        <td>23/05/20</td>
+                        <td>AF23048421</td>
+                        <td>3/3</td>
+                        <td className="icon-text-td">
+                          <FontAwesomeIcon
+                            icon={faCircle}
+                            color="#2FE784"
+                            fontSize={7}
+                            style={{
+                              display: "flex",
+                              alignItems: "center",
+                              justifyContent: "center",
+                            }}
+                          />
+                          Complete
+                        </td>
+                        <td>View</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+
+              <div className="kyc_status_charts_container">
+                <div className="kyc_status_charts_header">Charts</div>
+
+                <div className="total_kyc_request_stats">
+                    <div className="total_kyc_request_label">
+                      Total KYC Request
+                      <br></br>
+                      <span><CountUp start={0} end={3000} duration={3} /></span>
+                    </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
